610e04f7435e6537b63adc8aa8fa54dc9fbe65f1
AllRedCat <leonismogabriel@gmail.com>
2023-12-10 01:20:42 -0300

    add theme switch buttons

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const themeOptions = document.querySelectorAll('.theme-option');
+
+    themeOptions.forEach(option => {
+        option.addEventListener('click', function () {
+            const selectedTheme = this.getAttribute('data-theme');
+            document.documentElement.setAttribute('data-theme', selectedTheme);
+        })
+    })
+})
+
 function appendToResult(value) {
     document.getElementById('result').value += value;
 }
